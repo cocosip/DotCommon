990bc2e166ea7e3e9245f6d26713512fb0d6cb8d
virtual <1185513330@qq.com>
2017-08-17 17:50:09 +0800

    修复bug
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ test/DotCommon.Test/Utility/PinYinUtilTest.cs
@@ -0,0 +1,29 @@
+﻿using DotCommon.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DotCommon.Test.Utility
+{
+    public class PinYinUtilTest
+    {
+
+        [Theory]
+        [InlineData("中国", "ZhongGuo")]
+        public void ConvertChTest(string chStr, string pinyinStr)
+        {
+            var actual = PinYinUtil.ConvertCh(chStr);
+            Assert.Equal(pinyinStr, actual);
+        }
+
+        [Theory]
+        [InlineData("中国", "Z")]
+        //[InlineData("美", "M")]
+        public void GetFirstLetterTest(string chStr, string letter)
+        {
+            var actual = PinYinUtil.GetFirstLetter(chStr);
+            Assert.Equal(letter, actual);
+        }
+    }
+}
