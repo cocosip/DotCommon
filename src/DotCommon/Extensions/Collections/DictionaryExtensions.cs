784d887e7157f38d7f45d6d463f3ccf25746ba97
virtual <1185513330@qq.com>
2017-11-15 21:50:54 +0800

    去掉Dictionary转json功能 Signed-off-by: virtual <1185513330@qq.com>

--- src/DotCommon/Extensions/Collections/DictionaryExtensions.cs
+++ src/DotCommon/Extensions/Collections/DictionaryExtensions.cs
@@ -2,8 +2,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace DotCommon.Extensions
 {
@@ -55,47 +53,10 @@ namespace DotCommon.Extensions
 
 
 
-        public static void Remove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dict, TKey key)
+        public static bool Remove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dict, TKey key)
         {
             TValue value;
-            dict.TryRemove(key, out value);
+            return dict.TryRemove(key, out value);
         }
-
-        /// <summary>将string,string类型字典转换成json
-        /// </summary>
-        public static string ToStringJson(this Dictionary<string, string> dictionary)
-        {
-            var sb = new StringBuilder();
-            sb.Append($@"{{");
-            foreach (var kv in dictionary.Where(kv => !string.IsNullOrWhiteSpace(kv.Key)))
-            {
-                sb.Append($@"""{kv.Key}""");
-                sb.Append($@":");
-                sb.Append($@"""{kv.Value}""");
-                sb.Append($@",");
-            }
-            sb.Remove(sb.Length - 1, 1);
-            sb.Append($"}}");
-            return sb.ToString();
-        }
-
-        /// <summary>将string,object类型字典转换成json
-        /// </summary>
-        public static string ToJson(this Dictionary<string, object> dictionary)
-        {
-            var sb = new StringBuilder();
-            sb.Append($@"{{");
-            foreach (var kv in dictionary.Where(kv => !string.IsNullOrWhiteSpace(kv.Key)))
-            {
-                sb.Append($@"""{kv.Key}""");
-                sb.Append($@":");
-                sb.Append(kv.Value.GetType().GetTypeInfo().IsValueType ? $@"{kv.Value}" : $@"""{kv.Value}""");
-                sb.Append($@",");
-            }
-            sb.Remove(sb.Length - 1, 1);
-            sb.Append($"}}");
-            return sb.ToString();
-        }
-
     }
 }
