c18f44d14d869a146db8b230082162a7181341fb
virtual <1185513330@qq.com>
2018-02-02 13:59:11 +0800

    增加异步方法同步化等待 Signed-off-by: virtual <1185513330@qq.com>

---
+++ src/DotCommon.Caching/Threading/AsyncHelper.cs
@@ -0,0 +1,28 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DotCommon.Threading
+{
+    public static class AsyncHelper
+    {
+        public static bool IsAsync(this MethodInfo method)
+        {
+            return (
+                method.ReturnType == typeof(Task) ||
+                (method.ReturnType.GetTypeInfo().IsGenericType && method.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
+            );
+        }
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return AsyncContext.Run(func);
+        }
+
+        public static void RunSync(Func<Task> action)
+        {
+            AsyncContext.Run(action);
+        }
+    }
+}
