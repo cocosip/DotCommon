de7aa8f3e201a9d56813f14377a6a34144cde2dd
virtual <1185513330@qq.com>
2018-04-10 22:14:37 +0800

    增加枚举描述到SortList转换 Signed-off-by: virtual <1185513330@qq.com>

--- src/DotCommon/Utility/EnumUtil.cs
+++ src/DotCommon/Utility/EnumUtil.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
 
 namespace DotCommon.Utility
 {
@@ -17,28 +21,69 @@ namespace DotCommon.Utility
         /// </summary>
         public static T FromStr<T>(string str)
         {
-            return (T) Enum.Parse(typeof (T), str);
+            return (T)Enum.Parse(typeof(T), str);
         }
 
         /// <summary>获取枚举中的全部类型的数量
         /// </summary>
         public static int GetEnumCount<T>()
         {
-            return Enum.GetNames(typeof (T)).Length;
+            return Enum.GetNames(typeof(T)).Length;
         }
 
         /// <summary>获取全部的枚举
         /// </summary>
         public static Array GetValues<T>()
         {
-            return Enum.GetValues(typeof (T));
+            return Enum.GetValues(typeof(T));
         }
 
         /// <summary>判断该值是否为指定枚举中的值
         /// </summary>
         public static bool InEnum<T>(object value)
         {
-            return Enum.IsDefined(typeof (T), value);
+            return Enum.IsDefined(typeof(T), value);
         }
+
+        /// <summary>获取枚举类型的描述集合
+        /// </summary>
+        public static SortedList<string, string> GetEnumItems<T>(bool isInt32Key = true)
+        {
+            var items = new SortedList<string, string>();
+            Array array = Enum.GetValues(typeof(T));
+            foreach (var item in array)
+            {
+                var key = isInt32Key ? Convert.ToInt32(item).ToString() : item.ToString();
+                var displayValue = item.ToString();
+                FieldInfo field = item.GetType().GetField(item.ToString());
+                object[] attrArray = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                //如果有Desceiption属性,返回Descriotion的值
+                if (attrArray != null && attrArray.Length > 0)
+                {
+                    DescriptionAttribute descriptionAttr = (DescriptionAttribute)attrArray[0];
+                    displayValue = descriptionAttr.Description;
+                }
+                items.Add(key, displayValue);
+            }
+            return items;
+        }
+
+        /// <summary>获取枚举的某个描述值
+        /// </summary>
+        public static string GetEnumDescription(Enum enumValue)
+        {
+            string value = enumValue.ToString();
+            FieldInfo field = enumValue.GetType().GetField(value);
+            object[] attrArray = field.GetCustomAttributes(typeof(DescriptionAttribute), false);    //获取描述属性
+            if (attrArray == null || attrArray.Length == 0)
+            {
+                return value;
+            }
+            DescriptionAttribute descriptionAttribute = (DescriptionAttribute)attrArray[0];
+            return descriptionAttribute.Description;
+        }
+
+
+
     }
 }
---
+++ test/DotCommon.Test/Utility/EnumUtilTest.cs
@@ -0,0 +1,54 @@
+﻿using DotCommon.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xunit;
+
+namespace DotCommon.Test.Utility
+{
+    public class EnumUtilTest
+    {
+        [Fact]
+        public void GetEnumItemsTest()
+        {
+            var list = EnumUtil.GetEnumItems<TestEnum>();
+            Assert.Equal("TE1", list["1"]);
+            Assert.Equal("这是E2", list["2"]);
+            Assert.Equal("E3Test", list["3"]);
+            Assert.Equal("E4", list["6"]);
+            Assert.Equal("TE5", list["7"]);
+        }
+
+
+        [Fact]
+        public void GetEnumDescription()
+        {
+            var e1 = EnumUtil.GetEnumDescription(TestEnum.E1);
+            var e2 = EnumUtil.GetEnumDescription(TestEnum.E2);
+            var e3 = EnumUtil.GetEnumDescription(TestEnum.E3);
+            var e4 = EnumUtil.GetEnumDescription(TestEnum.E4);
+            var e5 = EnumUtil.GetEnumDescription(TestEnum.E5);
+            Assert.Equal("TE1", e1);
+            Assert.Equal("这是E2", e2);
+            Assert.Equal("E3Test", e3);
+            Assert.Equal("E4", e4);
+            Assert.Equal("TE5", e5);
+        }
+
+    }
+
+    public enum TestEnum
+    {
+        [Description("TE1")]
+        E1 = 1,
+        [Description("这是E2")]
+        E2,
+        [Description("E3Test")]
+        E3,
+        E4 = 6,
+        [Description("TE5")]
+        E5
+
+    }
+}
