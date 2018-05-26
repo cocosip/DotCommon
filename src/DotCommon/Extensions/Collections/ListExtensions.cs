3d4e5c0936200dddb29dfc8429cb19ef49b753f7
virtual <1185513330@qq.com>
2018-05-26 08:44:30 +0800

    更新List扩展
    Signed-off-by: virtual <1185513330@qq.com>

--- src/DotCommon/Extensions/Collections/ListExtensions.cs
+++ src/DotCommon/Extensions/Collections/ListExtensions.cs
@@ -1,57 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DotCommon.Extensions
 {
     public static class ListExtensions
     {
-
-        public static List<T> SortByDependencies<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> getDependencies)
+        /// <summary>移除某个项
+        /// </summary>
+        public static bool Remove<T>(this List<T> collection, Func<T, bool> predicate)
         {
-            /* See: http://www.codeproject.com/Articles/869059/Topological-sorting-in-Csharp
-             *      http://en.wikipedia.org/wiki/Topological_sorting
-             */
-
-            var sorted = new List<T>();
-            var visited = new Dictionary<T, bool>();
-
-            foreach (var item in source)
-            {
-                SortByDependenciesVisit(item, getDependencies, sorted, visited);
-            }
-
-            return sorted;
+            return collection.Remove(collection.FirstOrDefault(predicate));
         }
 
-
-        private static void SortByDependenciesVisit<T>(T item, Func<T, IEnumerable<T>> getDependencies, List<T> sorted, Dictionary<T, bool> visited)
-        {
-            bool inProcess;
-            var alreadyVisited = visited.TryGetValue(item, out inProcess);
-
-            if (alreadyVisited)
-            {
-                if (inProcess)
-                {
-                    throw new ArgumentException("Cyclic dependency found! Item: " + item);
-                }
-            }
-            else
-            {
-                visited[item] = true;
-
-                var dependencies = getDependencies(item);
-                if (dependencies != null)
-                {
-                    foreach (var dependency in dependencies)
-                    {
-                        SortByDependenciesVisit(dependency, getDependencies, sorted, visited);
-                    }
-                }
-
-                visited[item] = false;
-                sorted.Add(item);
-            }
-        }
     }
 }
