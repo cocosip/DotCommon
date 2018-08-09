a994a012dcf3a8957e43fc98be3e80a7c58ff4a4
virtual <1185513330@qq.com>
2018-08-09 14:40:45 +0800

    增加文件夹递归拷贝的方法 Signed-off-by: virtual <1185513330@qq.com>

--- src/DotCommon/IO/DirectoryHelper.cs
+++ src/DotCommon/IO/DirectoryHelper.cs
@@ -13,5 +13,31 @@ namespace DotCommon.IO
                 Directory.CreateDirectory(directory);
             }
         }
+
+        /// <summary>拷贝文件夹和文件夹下的文件
+        /// </summary>
+        public static void CopyDir(string sourceDir, string targetDir)
+        {
+
+            CreateIfNotExists(targetDir);
+            DirectoryInfo dir = new DirectoryInfo(sourceDir);
+            FileSystemInfo[] fileinfo = dir.GetFileSystemInfos();  //获取目录下（不包含子目录）的文件和子目录
+            foreach (FileSystemInfo i in fileinfo)
+            {
+                //判断是否文件夹
+                if (i is DirectoryInfo)
+                {
+                    //递归调用复制子文件夹
+                    CopyDir(i.FullName, Path.Combine(targetDir, i.Name));
+                }
+                else
+                {
+                    //拷贝文件,覆盖的形式
+                    File.Copy(i.FullName, Path.Combine(targetDir, i.Name), true);
+                }
+            }
+
+        }
+
     }
 }
