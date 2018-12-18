71ac7dd486f978fa0a5c5dd2374433e1c2002167
virtual <1185513330@qq.com>
2018-12-18 13:00:25 +0800

    移除ImageResizer
    Signed-off-by: virtual <1185513330@qq.com>

--- samples/DotCommon.AspNetCore.Mvc.Demo/Startup.cs
+++ samples/DotCommon.AspNetCore.Mvc.Demo/Startup.cs
@@ -2,7 +2,6 @@
 using DotCommon.AspNetCore.Mvc.Demo.Controllers;
 using DotCommon.AspNetCore.Mvc.Demo.Services;
 using DotCommon.AspNetCore.Mvc.Formatters;
-using DotCommon.AspNetCore.Mvc.ImageResizer;
 using DotCommon.Caching;
 using DotCommon.DependencyInjection;
 using DotCommon.Json4Net;
@@ -44,7 +43,6 @@ namespace DotCommon.AspNetCore.Mvc.Demo
                 .AddJson4Net()
                 .AddGenericsMemoryCache() //自定义缓存
                 .AddWildcardCors("http://*.cnblog.com,http://www.baidu.com") //通配符跨域
-                .AddImageResizer()
                 .AddRawRequestBodyFormatter()  //Get([Frombody]string name)
                 .AddServiceControllers(o =>
                 {
@@ -73,14 +71,6 @@ namespace DotCommon.AspNetCore.Mvc.Demo
 
             app.UseMvc();
             app.ConfigureDotCommon();
-            app.UseImageResizer();
-
-            //var a = app.ApplicationServices.GetRequiredService<ApplicationPartManager>();
-
-
-            //app.ApplicationServices.UseDynamicControllers();
-
-            //app.InitializeApplication();
         }
 
     }
