883daa724510d4fb2dc972e9f22ec591308780c0
virtual <1185513330@qq.com>
2019-02-14 17:00:55 +0800

    分离AsyncHelper类,DotCommon中为基础功能。
    Signed-off-by: virtual <1185513330@qq.com>

--- src/DotCommon.Caching/Threading/AsyncHelper.cs
+++ src/DotCommon.Caching/Threading/AsyncHelper.cs
@@ -7,33 +7,6 @@ namespace DotCommon.Threading
 {
     public static class AsyncHelper
     {
-        /// <summary>检测是否为异步方法
-        /// </summary>
-        public static bool IsAsync(this MethodInfo method)
-        {
-            return method.ReturnType.IsTaskOrTaskOfT();
-        }
-
-        public static bool IsTaskOrTaskOfT(this Type type)
-        {
-            return type == typeof(Task) || type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Task<>);
-        }
-
-        public static Type UnwrapTask( Type type)
-        {
-            if (type == typeof(Task))
-            {
-                return typeof(void);
-            }
-
-            if (type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Task<>))
-            {
-                return type.GenericTypeArguments[0];
-            }
-
-            return type;
-        }
-
         public static TResult RunSync<TResult>(Func<Task<TResult>> func)
         {
             return AsyncContext.Run(func);
