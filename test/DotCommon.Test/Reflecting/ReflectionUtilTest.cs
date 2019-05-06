b74c545d199df32d5294476a88a85a5ad739bb35
virtual <1185513330@qq.com>
2019-05-06 17:14:27 +0800

    增加是否为某个泛型的接口或者泛型类的实现的判断 测试代码
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ test/DotCommon.Test/Reflecting/ReflectionUtilTest.cs
@@ -0,0 +1,53 @@
+﻿using DotCommon.Reflecting;
+using Xunit;
+
+namespace DotCommon.Test.Reflecting
+{
+    public class ReflectionUtilTest
+    {
+        [Fact]
+        public void IsAssignableToGenericType_Test()
+        {
+            var t3Type = typeof(GenericClassTest3);
+            Assert.True(ReflectionUtil.IsAssignableToGenericType(t3Type, typeof(ITestGenericInterface<>)));
+
+            var t4Type = typeof(GenericClassTest4);
+            Assert.True(ReflectionUtil.IsAssignableToGenericType(t4Type, typeof(TestGenericClass<>)));
+        }
+
+    }
+
+    interface ITestGenericInterface<T>
+    {
+
+    }
+
+    class TestGenericClass<T>
+    {
+
+    }
+    class GenericClassT
+    {
+
+    }
+    class GenericClassTest1<T> : ITestGenericInterface<T>
+    {
+
+    }
+
+    class GenericClassTest2<T> : TestGenericClass<T>
+    {
+
+    }
+
+    class GenericClassTest3 : GenericClassTest1<GenericClassT>
+    {
+
+    }
+
+    class GenericClassTest4 : GenericClassTest2<GenericClassT>
+    {
+
+    }
+
+}
