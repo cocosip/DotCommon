b0cf4efbe600702549868261fa1c85c3a7d17a34
virtual <1185513330@qq.com>
2019-07-08 11:16:27 +0800

    增加雪花算法,生成唯一的Id Signed-off-by: virtual <1185513330@qq.com>

---
+++ test/DotCommon.Test/Utility/SnowflakeTest.cs
@@ -0,0 +1,25 @@
+﻿using DotCommon.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DotCommon.Test.Utility
+{
+    public class SnowflakeTest
+    {
+        [Fact]
+        public void Snowflake_Test()
+        {
+            var snowflake = new SnowflakeDistributeId(1);
+
+            var v1 = snowflake.NextId();
+            var v2 = snowflake.NextId();
+
+            Assert.True(v1 > 0);
+            Assert.True(v2 > 0);
+
+
+        }
+    }
+}
