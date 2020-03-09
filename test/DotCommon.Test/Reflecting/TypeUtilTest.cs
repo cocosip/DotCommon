b57eee2434598dd9c011a6b600536c53422588d9
virtual <1185513330@qq.com>
2020-03-09 14:20:06 +0800

    移除无用测试,修复因TaskFactory扩展引起的单元测试加载不正常的bug Signed-off-by: virtual <1185513330@qq.com>

--- test/DotCommon.Test/Reflecting/TypeUtilTest.cs
+++ test/DotCommon.Test/Reflecting/TypeUtilTest.cs
@@ -1,7 +1,9 @@
 ﻿using DotCommon.Reflecting;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DotCommon.Test.Reflecting
@@ -22,10 +24,50 @@ namespace DotCommon.Test.Reflecting
                 return DateTime.Now;
             };
 
+
+            Action<int> action1 = a => { };
+
             Assert.True(TypeUtil.IsFunc(func1));
             Assert.False(TypeUtil.IsFunc(func2));
             Assert.True(TypeUtil.IsFunc(func3));
             Assert.True(TypeUtil.IsFunc<int>(func1));
+            Assert.False(TypeUtil.IsFunc(action1));
+        }
+
+        [Fact]
+        public void IsAsync_Test()
+        {
+            var method1 = this.GetType().GetMethod("AsyncMethod1", BindingFlags.NonPublic | BindingFlags.Instance);
+            var method2 = this.GetType().GetMethod("SyncMethod1", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Assert.True(TypeUtil.IsAsync(method1));
+            Assert.False(TypeUtil.IsAsync(method2));
+        }
+
+        private void SyncMethod1()
+        {
+
+        }
+
+        private Task AsyncMethod1()
+        {
+            return Task.FromResult(1);
+        }
+
+
+       
+
+    }
+
+    public class TypeUtilClass1
+    {
+
+    }
+
+    public static class TypeUtilClass1Extension
+    {
+        public static void Method1(this TypeUtilClass1 typeUtilClass1)
+        {
 
         }
     }
