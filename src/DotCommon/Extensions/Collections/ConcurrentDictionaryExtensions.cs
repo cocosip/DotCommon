9d59db3d93d757dc6dc63db26481f7d53878a2c0
virtual <1185513330@qq.com>
2021-01-27 16:41:59 +0800

    Upgrade package
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ src/DotCommon/Extensions/Collections/ConcurrentDictionaryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Concurrent;
+
+namespace DotCommon.Extensions
+{
+    public static class ConcurrentDictionaryExtensions
+    {
+        public static bool Remove<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dict, TKey key)
+        {
+            return dict.TryRemove(key, out _);
+        }
+    }
+}
