c9487a03a07fd688a9fdb3f262accbfb301b6984
virtual <1185513330@qq.com>
2021-11-02 13:42:59 +0800

    Add GetFileSize method
    Signed-off-by: virtual <1185513330@qq.com>

--- src/DotCommon/IO/FileHelper.cs
+++ src/DotCommon/IO/FileHelper.cs
@@ -10,12 +10,24 @@ namespace DotCommon.IO
         /// <summary>
         /// 如果文件存在就删除文件
         /// </summary>
-        public static void DeleteIfExists(string filePath)
+        /// <param name="fileName">文件名</param>
+        public static void DeleteIfExists(string fileName)
         {
-            if (File.Exists(filePath))
+            if (File.Exists(fileName))
             {
-                File.Delete(filePath);
+                File.Delete(fileName);
             }
+        }
+
+        /// <summary>
+        /// 获取文件的大小
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static long GetFileSize(string fileName)
+        {
+            var fileInfo = new FileInfo(fileName);
+            return fileInfo.Length;
         }
     }
 }
