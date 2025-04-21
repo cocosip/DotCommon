494220545ab1c202a4d6f299c9903f9d46555d70
virtual <1185513330@qq.com>
2025-04-21 17:24:10 +0800

    add missing aspnetcore mvc
    Signed-off-by: virtual <1185513330@qq.com>

---
+++ src/DotCommon.AspNetCore.Mvc/Cors/WildcardCorsServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotCommon.AspNetCore.Mvc.Cors
+{
+    /// <summary>通配符跨域服务扩展
+    /// </summary>
+    public static class WildcardCorsServiceExtensions
+    {
+        /// <summary>添加通配符跨域
+        /// </summary>
+        public static IServiceCollection AddWildcardCors(this IServiceCollection services, string origins, Action<CorsPolicyBuilder>? configure = null)
+        {
+            services.Add(ServiceDescriptor.Transient<ICorsService, WildcardCorsService>());
+            services.Configure<CorsOptions>(options => options.AddPolicy(WildcardCorsService.WildcardCorsPolicyName, builder =>
+            {
+                var originArray = origins.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(o => o.RemovePostFix("/")).ToArray();
+                configure?.Invoke(builder);
+                if (configure == null)
+                {
+                    builder.WithOrigins(originArray)
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                }
+            }));
+
+            return services;
+        }
+    }
+}
---
+++ src/DotCommon.AspNetCore.Mvc/Formatters/RawRequestBodyFormatter.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DotCommon.AspNetCore.Mvc.Formatters
+{
+    /// <summary>
+    /// Formatter that allows content of type text/plain and application/octet stream
+    /// or no content type to be parsed to raw data. Allows for a single input parameter
+    /// in the form of:
+    /// 
+    /// public string RawString([FromBody] string data)
+    /// public byte[] RawData([FromBody] byte[] data)
+    /// </summary>
+    public class RawRequestBodyFormatter : InputFormatter
+    {
+        /// <summary>Ctor
+        /// </summary>
+        public RawRequestBodyFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/plain"));
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/octet-stream"));
+        }
+
+
+        /// <summary>
+        /// Allow text/plain, application/octet-stream and no content type to
+        /// be processed
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override bool CanRead(InputFormatterContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            var contentType = context.HttpContext.Request.ContentType;
+            if (string.IsNullOrEmpty(contentType) || contentType == "text/plain" ||
+                contentType == "application/octet-stream")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Handle text/plain or no content type for string results
+        /// Handle application/octet-stream for byte[] results
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            var request = context.HttpContext.Request;
+            var contentType = context.HttpContext.Request.ContentType;
+
+
+            if (string.IsNullOrEmpty(contentType) || contentType == "text/plain")
+            {
+                using (var reader = new StreamReader(request.Body))
+                {
+                    var content = await reader.ReadToEndAsync();
+                    return await InputFormatterResult.SuccessAsync(content);
+                }
+            }
+            if (contentType == "application/octet-stream")
+            {
+                using (var ms = new MemoryStream(2048))
+                {
+                    await request.Body.CopyToAsync(ms);
+                    var content = ms.ToArray();
+                    return await InputFormatterResult.SuccessAsync(content);
+                }
+            }
+
+            return await InputFormatterResult.FailureAsync();
+        }
+    }
+}
